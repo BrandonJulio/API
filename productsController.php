2aa426e63976a71a84b5156fd58de60f49531fb0
Brandon Julio <bjulio@unicesar.edu.co>
2025-08-31 13:34:32 -0500

    feat:verificar la longitud de los campos name, description y price verificar el parámetro price sea de tipo numérico

--- productsController.php
+++ productsController.php
@@ -1,4 +1,8 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+    header('Allow: GET, POST, PUT, DELETE');
     header('content-type: application/json; charset=utf-8');
     require_once 'productsModel.php';
     $productsModel = new productsModel();
@@ -9,14 +13,14 @@
         break;
         case 'POST':
             $_POST = json_decode (file_get_contents('php://input', true));
-            if(!isset($_POST->name) || is_null($_POST->name) || empty(trim($_POST->name))){
-                $respuesta =['error','El nombre del producto esta vacio'];
+            if(!isset($_POST->name) || is_null($_POST->name) || empty(trim($_POST->name)) || strlen($_POST->name)>80){
+                $respuesta =['error','El nombre del producto esta vacio y no debe de exceder los 80 caracteres'];
             }
-            else if(!isset($_POST -> description)|| is_null($_POST->description) || empty(trim($_POST->description))){
-                $respuesta = ['error','La descripcion del producto esta vacia'];
+            else if(!isset($_POST -> description)|| is_null($_POST->description) || empty(trim($_POST->description))|| strlen($_POST->description)>150){
+                $respuesta = ['error','La descripcion del producto esta vacia y no debe de exceder los 150 caracteres'];
             }
-            else if(!isset($_POST->price) || is_null($_POST->price) || empty(trim($_POST->price))){
-                $respuesta = ['error','El precio del producto esta vacio'];
+            else if(!isset($_POST->price) || is_null($_POST->price) || empty(trim($_POST->price))|| !is_numeric($_POST->price)|| strlen($_POST->price)>20){
+                $respuesta = ['error','El precio del producto esta vacio, debe de ser tipo numérico y no tener más de 20 caracteres'];
             }
             else{
                 $respuesta = $productsModel->saveProducts($_POST->name,$_POST->description,$_POST->price);
@@ -26,16 +30,16 @@
         case 'PUT':
             $_PUT = json_decode(file_get_contents('php://input',true));
             if(!isset($_PUT->id) || is_null($_PUT->id) || empty(trim($_PUT->id))){
-                $respuesta =['error','El id del producto esta vacio'];
+                $respuesta =['error','El ID del producto esta vacio'];
             }
-            else if(!isset($_PUT->name) || is_null($_PUT->name) || empty(trim($_PUT->name))){
-                $respuesta =['error','El nombre del producto esta vacio'];
+            else if(!isset($_PUT->name) || is_null($_PUT->name) || empty(trim($_PUT->name))|| strlen($_POST->name)>80){
+                $respuesta =['error','El nombre del producto esta vacio y no debe de exceder los 80 caracteres'];
                 }
-            else if(!isset($_PUT -> description)|| is_null($_PUT->description) || empty(trim($_PUT->description))){
-                $respuesta = ['error','La descripcion del producto esta vacia'];
+            else if(!isset($_PUT -> description)|| is_null($_PUT->description) || empty(trim($_PUT->description))|| strlen($_POST->description)>150){
+                $respuesta = ['error','La descripcion del producto esta vacia y no debe de exceder los 150 caracteres'];
             }
-                else if(!isset($_PUT->price) || is_null($_PUT->price) || empty(trim($_PUT->price))){
-                $respuesta = ['error','El precio del producto esta vacio'];
+                else if(!isset($_PUT->price) || is_null($_PUT->price) || empty(trim($_PUT->price))|| !is_numeric($_PUT->price)|| strlen($_POST->price)>20){
+                $respuesta = ['error','El precio del producto esta vacio, debe de ser tipo numérico y no tener más de 20 caracteres'];
             }
             else{
                 $respuesta = $productsModel->updateProducts($_PUT->id,$_PUT->name,$_PUT->description,$_PUT->price);
@@ -50,7 +54,7 @@
             else{
                 $respuesta = $productsModel->deleteProducts($_DELETE->id);
             }
-            echo json_encode($respuesta);
+            echo json_encode($respuesta); 
             break;
     }
 ?>
